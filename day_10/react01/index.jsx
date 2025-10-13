e5468e4cc807d795da2d5fdbd77d4b6b3479debc
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-10-13 20:45:24 +0530

    react

--- day_10/react01/index.jsx
+++ day_10/react01/index.jsx
@@ -5,18 +5,16 @@
         <h1 id="heading3"></h1>
         <h1 id="heading4"></h1>
     </div>
-</div> */}
-
-
+</div> */
+}
 const heading = React.createElement("div", { id: "outer-div" },
     React.createElement("div", { id: "inner-div" },
         [React.createElement("h1", { id: "heading1" }, "Hello! React Learners"),
         React.createElement("h1", { id: "heading2" }, "Hello! React Learners"),
         React.createElement("h1", { id: "heading3" }, "Hello! React Learners"),
-        React.createElement("h1", { id: "heading4" }, "Hello! React Learners")]
+        React.createElement("h1", { id: "heading4" },)]
     )
 );
 console.log(heading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
